ba80bcee271bfc9344465ddcfa411653d4ce385a
Temidayojnr <temidayojunior@gmail.com>
2019-07-29 10:43:44 +0100

    Initial commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Article;
+use App\Http\Resources\Article as ArticleResource;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::paginate(15);
+
+        return ArticleResource::collection($articles);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $article = $request->isMethod('put') ? Article::findOrFail($request->id) : new Article;
+
+        $article->id = $request->id;
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+        if ($article->save()) {
+            return new ArticleResource($article);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // show article
+        $article = Article::findOrFail($id);
+
+        return new ArticleResource($article);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $article = Article::findOrFail($id);
+
+        if ($article->delete()) {
+            return new ArticleResource($article);
+        }
+    }
+}
